bbc7154b17c9c5d31289998cc0de716b69881dac
Kornelis Sietsma <korny@sietsma.com>
2017-09-15 09:24:28 +0100

    BAU: adding a precommit with some tests
    note you need jq installed - the script tests for this, but not for the jq version.
    
    solo: @kornysietsma

---
+++ precommit.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -eu
+
+command -v jq >/dev/null 2>&1 || { echo >&2 "You need jq installed to run this."; exit 1; }
+
+echo "parsing users.json"
+
+jq '. | { parsed: "ok" }' users.json
+
+echo "testing unique fields"
+
+function check_unique_field() {
+  local all_vals
+  local unique_vals
+  if [ $# -ne 1 ]; then
+    echo "invalid arguments"
+    exit 1
+  fi
+
+  all_vals=$(jq -r ".[].$1" users.json | wc -l)
+  unique_vals=$(jq -r ".[].$1" users.json | sort | uniq | wc -l)
+
+  if [ "$all_vals" != "$unique_vals" ]; then
+    echo "There are non-unique values of \"$1\" in the users.json file:"
+    echo $(diff <(jq -r ".[].$1" users.json | sort) <(jq -r ".[].$1" users.json | sort | uniq))
+    exit -1
+  fi
+}
+
+function check_unique_array_field() {
+  local all_vals
+  local unique_vals
+  if [ $# -ne 1 ]; then
+    echo "invalid arguments"
+    exit 1
+  fi
+
+  all_vals=$(jq -r "[.[].$1] | flatten | .[]" users.json | wc -l)
+  unique_vals=$(jq -r "[.[].$1] | flatten | .[]" users.json | sort | uniq | wc -l)
+
+  if [ "$all_vals" != "$unique_vals" ]; then
+    echo "There are non-unique values of \"$1\" in the users.json file:"
+    echo $(diff <(jq -r "[.[].$1] | flatten | .[]" users.json | sort) <(jq -r "[.[].$1] | flatten | .[]" users.json | sort | uniq))
+    exit -1
+  fi
+}
+
+check_unique_field 'slack_id'
+check_unique_field 'slack_name'
+check_unique_array_field 'emails'
+check_unique_array_field 'names'
+
+echo "all ok."
