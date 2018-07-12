74d44cfc0abeae2dcbc52dfdd58a2e703065ea29
Anshul Sirur <anshul.sirur@digital.cabinet-office.gov.uk>
2018-07-12 17:05:50 +0100

    BAU: Update provision script
    After provisioning the repository resource:
    
    - Remove any unmanaged collaborators' access to the repository
    - Remove the old Identity Assurance [Branchers] and Reliability
    Engineering [Branchers] teams from the repo
    
    Author: @vixus0

--- tf_github/provision.rb
+++ tf_github/provision.rb
@@ -7,8 +7,14 @@ if ARGV.size < 1
   abort('Usage: ./provision.rb repo')
 end
 
+repo = ARGV[0]
+
 client = Octokit::Client.new(access_token: ENV.fetch('TF_VAR_github_token'))
-client.protect_branch("#{ARGV[0]}", 'master', {
+client.auto_paginate = true
+
+# Until the TF Github provider supports required_approving_review_count
+# we need to apply branch protection here.
+client.protect_branch("#{repo}", 'master', {
   accept: 'application/vnd.github.luke-cage-preview+json',
   required_status_checks: nil,
   enforce_admins: nil,
@@ -19,3 +25,12 @@ client.protect_branch("#{ARGV[0]}", 'master', {
     require_code_owner_reviews: false
   }
 })
+
+# Remove any unmanaged collaborators
+client.collaborators(repo).each { |user| client.remove_collaborator(repo, user) }
+
+# Remove old IDA teams
+old_teams = ['team-identity-assurance', 'team-identity-assurance-branchers', 'reliability-engineering', 'reliability-engineering-branchers']
+client.org_teams('alphagov').select { |team| old_teams.include?(team.slug) }.map(&:id).each do |id|
+  client.remove_team_repo(id, repo)
+end
