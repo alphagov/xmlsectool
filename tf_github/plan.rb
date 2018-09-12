6ec215e830474fbdaa0859e863f364f689da27cd
Anshul Sirur <anshul.sirur@digital.cabinet-office.gov.uk>
2018-09-12 11:03:00 +0100

    Rename apply.rb to plan.rb

--- tf_github/plan.rb
+++ tf_github/plan.rb
@@ -3,13 +3,12 @@
 require 'optparse'
 
 ARGV.options do |opts|
-  opts.on('--apply', 'Run terraform apply') { @apply = true }
   opts.on_tail('-h', '--help') { abort(opts.to_s) }
   opts.parse!
 end
 
 CONFIGS = ARGV.empty? ? ['teams', 'users', 'repos'] : ARGV
-LOGFILE = "tf_apply_#{Time.now.strftime('%Y%m%d-%H%M')}.#{@apply ? 'apply' : 'plan'}.log"
+LOGFILE = "tf_plan_#{Time.now.strftime('%Y%m%d-%H%M')}.log"
 
 def run(cmd)
   status = system(cmd, [:out, :err] => [LOGFILE, 'a'])
@@ -39,14 +38,8 @@ CONFIGS.each do |cfg|
   run("terraform plan -no-color -state=#{statefile} -out=#{planfile} #{cfg}")
   puts(`terraform plan #{planfile}`.split("\n").select { |l| l.include?('Plan:') })
 
-  
   File.delete('terraform.tfstate') if File.exist?('terraform.tfstate')
   File.delete('terraform.tfstate.backup') if File.exist?('terraform.tfstate.backup')
-
-  if @apply
-    puts(" - **APPLYING** plan #{planfile} using state #{statefile}")
-    run("terraform apply -no-color -state-out=#{statefile} #{planfile}")
-  end
 
   puts(" - DONE\n\n")
 end
