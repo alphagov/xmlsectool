f42778fecf6ab2aa96a01ebae45ba4929d04fda3
James Howes <james.howes@digital.cabinet-office.gov.uk>
2018-10-01 13:35:34 +0100

    bau - remove verify developers who have now left GDS

--- tf_github/users/user_felixmarcusmillne.tf
+++
@@ -1,8 +0,0 @@
-module "user_felixmarcusmillne" {
-  source = "./verify_user"
-
-  username = "FelixMarcusMillne"
-  realname = "Felix Millne"
-  org_role = "member"
-  teams = [ "${module.mission_eidas.teams}" ]
-}
--- tf_github/users/user_rachelthecodesmith.tf
+++
@@ -1,8 +0,0 @@
-module "user_rachelthecodesmith" {
-  source = "./verify_user"
-
-  username = "rachelthecodesmith"
-  realname = "Rachel Smith"
-  org_role = "member"
-  teams = [ "${module.mission_optimising.teams}" ]
-}
