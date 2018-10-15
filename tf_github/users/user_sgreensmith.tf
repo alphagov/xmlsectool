d1524e2e9357c8a5a69c95ca6f9ca688e1ed5507
Simon Worthington <simon.worthington@digital.cabinet-office.gov.uk>
2018-10-15 17:16:38 +0100

    BAU: Remove Simon Greensmith whilst not working on Verify
    Co-authored-by: Christopher Holmes <christopher.holmes@digital.cabinet-office.gov.uk>

--- tf_github/users/user_sgreensmith.tf
+++
@@ -1,8 +0,0 @@
-module "user_sgreensmith" {
-  source = "./verify_user"
-
-  username = "sgreensmith"
-  realname = "Simon Greensmith"
-  org_role = "member"
-  teams = [ "${module.mission_connecting.teams}" ]
-}
