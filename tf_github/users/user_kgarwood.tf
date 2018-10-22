73cd30f0bb8f9802edec63dd8fdc09b04d46b2d9
Simon Worthington <simon.worthington@digital.cabinet-office.gov.uk>
2018-10-22 15:56:47 +0100

    BAU: Remove Kevin Garwood.

--- tf_github/users/user_kgarwood.tf
+++
@@ -1,8 +0,0 @@
-module "user_kgarwood" {
-  source = "./verify_user"
-
-  username = "kgarwood"
-  realname = "Kevin Garwood"
-  org_role = "member"
-  teams = [ "${module.mission_eidas.teams}" ]
-}
