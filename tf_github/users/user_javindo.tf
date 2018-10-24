b16aba2670e14ea589180da6c31e72909df817e9
Chris Clayson <chris.clayson@digital.cabinet-office.gov.uk>
2018-10-24 14:32:15 +0100

    BAU: Remove Steve Butler

--- tf_github/users/user_javindo.tf
+++
@@ -1,8 +0,0 @@
-module "user_javindo" {
-  source = "./verify_user"
-
-  username = "javindo"
-  realname = "Steve Butler"
-  org_role = "member"
-  teams = [ "${module.mission_optimising.teams}" ]
-}
