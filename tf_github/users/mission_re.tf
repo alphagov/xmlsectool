11f01ca70e52487973d4da9e29d3b2e6d8ef9fb8
Anshul Sirur <anshul.sirur@digital.cabinet-office.gov.uk>
2018-11-21 11:12:51 +0000

    apply users: add core hub to RE mission

--- tf_github/users/mission_re.tf
+++ tf_github/users/mission_re.tf
@@ -1,11 +1,11 @@
 module "mission_re" {
   source = "./verify_mission"
   teams = [
-    "verify-tech-team-core-hub",
     "verify-tech-team-connecting",
     "verify-tech-team-dcs",
     "verify-tech-team-eidas-middleware",
     "verify-tech-team-infrastructure",
     "verify-tech-team-tools",
+    "verify-tech-team-core-hub",
   ]
 }
