6e80fb8e202607d6d705bcde99e34cfafe1a0532
James Howes <james.howes@digital.cabinet-office.gov.uk>
2018-12-03 10:40:21 +0000

    bau - remove andrew chong and irene lau

--- tf_github/users/user_andrewchong.tf
+++
@@ -1,8 +0,0 @@
-module "user_andrewchong" {
-  source = "./verify_user"
-
-  username = "andrewchong"
-  realname = "Andrew Chong"
-  org_role = "member"
-  teams = [ "${module.mission_eidas.teams}" ]
-}
