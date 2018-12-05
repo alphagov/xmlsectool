d1d73f127e57f2551ee5d5833e3098524d23691c
Anshul Sirur <vixus0@gmail.com>
2018-12-05 13:50:36 +0000

    apply repos: add verify-eidas-mw-run repo

---
+++ tf_github/repos/repo_verify_eidas_mw_run.tf
@@ -0,0 +1,10 @@
+module "repo_verify_eidas_mw_run" {
+  source = "./verify_repo"
+
+  name = "verify-eidas-mw-run"
+  description = "German eIDAS middleware run container"
+  private = "false"
+  push_teams = [
+        "verify-tech-team-eidas-middleware",
+      ]
+}
