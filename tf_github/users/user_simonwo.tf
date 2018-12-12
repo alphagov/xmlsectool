97f023ea649f9ec4ba4b10b962414e6a652ac658
James Howes <james.howes@digital.cabinet-office.gov.uk>
2018-12-12 08:56:20 +0000

    bau - remove simon worthington

--- tf_github/users/user_simonwo.tf
+++
@@ -1,8 +0,0 @@
-module "user_simonwo" {
-  source = "./verify_user"
-
-  username = "simonwo"
-  realname = "Simon Worthington"
-  org_role = "member"
-  teams = [ "${module.mission_eidas.teams}" ]
-}
