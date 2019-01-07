7187beee5bc0518c3810043208cd59f1145b0713
Tom Rosier <tom.rosier@digital.cabinet-office.gov.uk>
2019-01-07 12:18:33 +0000

    Added Phillip Miller

--- tf_github/users/user_irenelau_gds.tf
+++
@@ -1,8 +0,0 @@
-module "user_irenelau_gds" {
-  source = "./verify_user"
-
-  username = "IreneLau-GDS"
-  realname = "Irene Lau"
-  org_role = "admin"
-  teams = [ "${module.mission_all.teams}" ]
-}
