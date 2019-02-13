ac7abdb9a0482cf759624ae1fd9c8977fc5c6c50
Kerr Rainey <kerr.rainey@gmail.com>
2019-02-13 16:27:41 +0000

    BAU: Remove partha

--- tf_github/users/user_parthagds.tf
+++
@@ -1,8 +0,0 @@
-module "user_parthagds" {
-  source = "./verify_user"
-
-  username = "parthagds"
-  realname = "Partha Mondal"
-  org_role = "member"
-  teams = [ "${data.github_team.core.id}" ]
-}
