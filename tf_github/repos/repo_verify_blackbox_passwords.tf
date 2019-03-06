dad22d595baa0ee0aa901cb44cb4547f58fdb91b
Jakub Miarka <jakub.miarka@digital.cabinet-office.gov.uk>
2019-03-06 14:45:08 +0000

    BAU: Add the option to not protect master branches
    In case of some repos we want to allow direct pushes to master.
    E.g. `verify-blackbox-passwords` due to its binary nature, or the `verify-release-logs`
    for automated log submits.

--- tf_github/repos/repo_verify_blackbox_passwords.tf
+++ tf_github/repos/repo_verify_blackbox_passwords.tf
@@ -4,6 +4,7 @@ module "repo_verify_blackbox_passwords" {
   name = "verify-blackbox-passwords"
   description = "Securely stored credentials for Verify"
   private = "true"
+  allow_push_to_master = "true"
   push_teams = [
         "verify-tech-team-core",
         "verify-tech-team-infrastructure",
--- tf_github/repos/repo_verify_release_logs.tf
+++ tf_github/repos/repo_verify_release_logs.tf
@@ -4,6 +4,7 @@ module "repo_verify_release_logs" {
   name = "verify-release-logs"
   description = "Logs of releases generated from the verify-release-automation tool"
   private = "true"
+  allow_push_to_master = "true"
   push_teams = [
         "verify-tech-team-core",
       ]
