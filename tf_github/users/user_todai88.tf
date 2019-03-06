bfc6714c7ae362eaeba7ee507ce3b4b40db476cd
Benjamin Gill <github@bjgill.net>
2019-03-06 12:50:11 +0000

    Update tf_github/users/user_todai88.tf
    Co-Authored-By: phantas <paula.valenca@digital.cabinet-office.gov.uk>

--- tf_github/users/user_todai88.tf
+++ tf_github/users/user_todai88.tf
@@ -2,7 +2,7 @@ module "user_todai88" {
   source = "./verify_user"
 
   username  = "todai88"
-  realname  = "Joakim Kakaei"
+  realname  = "Joakim Bajoul Kakaei"
   can_merge = "false"
 
   teams = ["${data.github_team.core.id}"]
