09cb8f6371725b836fbf088c2bc0853aabb6e4bf
brenetic <b@brenetic.com>
2019-04-26 14:55:52 +0100

    Add Event System DB scripts repo
    This repo will be home to scripts related to the Event System database.

---
+++ tf_github/repos/repo_verify_event_system_database_scripts.tf
@@ -0,0 +1,10 @@
+module "repo_verify_event_system_database_scripts" {
+  source = "./verify_repo"
+
+  name = "verify-event-system-database-scripts"
+  description = "SQL scripts and schemas for the Event System database"
+  private = "false"
+  push_teams = [
+        "verify-tech-team-core-hub",
+      ]
+}
