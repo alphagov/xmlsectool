05c110d6868f2d9ae4f667c0777c70b8eb5287cb
Alex Monk <alex.monk@digital.cabinet-office.gov.uk>
2019-06-28 11:27:06 +0100

    Remove Mark Hynes
    Per Phil

--- tf_github/users/user_mmhmm.tf
+++
@@ -1,9 +0,0 @@
-module "user_mmhmm" {
-  source = "./verify_user"
-
-  username  = "mmhmm"
-  realname  = "Mark Hynes"
-  can_merge = "true"
-
-  teams = ["${data.github_team.infrastructure.id}"]
-}
